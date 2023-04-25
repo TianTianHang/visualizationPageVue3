1da041ebbb3e2b702932b5bbdc98fe2c34c58d48
tiantian <a2450804878@hotmail.com>
2023-04-25 22:36:44 +0800

    变更

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
@@ -48,5 +47,8 @@ export default defineConfig({
         }
       }
     }
+  },
+  build: {
+    outDir: './root'
   }
 })
